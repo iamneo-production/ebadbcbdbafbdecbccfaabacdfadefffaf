f0bdd10cab8a8cce634107018e7c14c91cc37d2a
Iamneo <techadmin@iamneo.ai>
2023-08-03 07:22:51 +0000

    Backup Commit

---
+++ Assignment2/TestAssignment2.java
@@ -0,0 +1,27 @@
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
+import io.cucumber.java.en.Then;
+
+public class GoogleSearchStepDefs {
+
+    @Given("I am on the Google homepage")
+    public void i_am_on_the_google_homepage() {
+        // Code to navigate to Google homepage
+    }
+
+    @When("I enter {string} in the search box")
+    public void i_enter_in_the_search_box(String searchQuery) {
+        // Code to enter searchQuery in the search box
+    }
+
+    @When("I click the \"Google Search\" button")
+    public void i_click_the_google_search_button() {
+        // Code to click on the "Google Search" button
+    }
+
+    @Then("I should see image results for {string}")
+    public void i_should_see_image_results_for(String searchQuery) {
+        // Code to verify that image results for searchQuery are displayed
+        // You can use appropriate assertions here
+    }
+}
