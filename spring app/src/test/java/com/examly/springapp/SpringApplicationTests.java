6f997583a08f08494c480058a8d6dc022068324c
Iamneo <techadmin@iamneo.ai>
2023-08-08 14:38:35 +0000

    Backup Commit

---
+++ spring app/src/test/java/com/examly/springapp/SpringApplicationTests.java
@@ -0,0 +1,67 @@
+package com.examly.springapp;
+
+import org.testng.annotations.Test;
+import java.net.URL;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.BeforeTest;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+
+public class SpringApplicationTests {
+
+    ChromeOptions chromeOptions = new ChromeOptions();
+    WebDriver driver = null;
+
+    @BeforeTest
+    public void beforeTest() throws Exception
+     {
+   // replace seleniumhost and port with correct values
+        driver = new RemoteWebDriver(new URL("http://<seleniumhost:port>/"), chromeOptions);
+        driver.manage().window().maximize();
+    }
+
+    public void openChromeBrowser() {
+        // Set ChromeDriver executable path
+        System.setProperty("webdriver.chrome.driver", "path/to/chromedriver");
+
+        // Disable Chrome browser notifications
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-notifications");
+
+        // Initialize ChromeDriver
+        driver = new ChromeDriver(options);
+    }
+
+    @When("I navigate to Google")
+    public void navigateToGoogle() {
+        driver.get("https://www.google.com/");
+    }
+
+    @When("I enter {string} in the search box")
+    public void enterSearchQuery(String query) {
+        driver.findElement(By.name("q")).sendKeys(query);
+    }
+
+    @When("I click the Google Images button")
+    public void clickGoogleImagesButton() {
+        driver.findElement(By.linkText("Images")).click();
+    }
+
+    @Then("I should see image results for {string}")
+    public void verifyImageResults(String query) {
+        String pageTitle = driver.getTitle();
+        assert pageTitle.contains(query + " - Google Search");
+    }
+
+    @AfterTest
+    public void afterTest() 
+    {
+        driver.quit();
+    }
+    
+}
